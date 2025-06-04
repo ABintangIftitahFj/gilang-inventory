e4768cdb2758b1fbf8dfd9ad49192465f8945b23
shzkaptrr <mauliaputri23@gmail.com>
2025-06-04 19:38:51 +0700

    add file scan.blade

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/scan', function () {
+    return view('scan');
+});
